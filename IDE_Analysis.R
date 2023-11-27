410dbe34e48f41914c7328d8603864ee3658ed18
Joseph Bradley <hermite2010@gmail.com>
2023-11-27 14:20:56 -0700

    added ARIMA code

--- IDE_Analysis.R
+++ IDE_Analysis.R
@@ -10,16 +10,20 @@ library(xgboost)    # FOR BOOSTED TREES
 library(parsnip)    # FOR BART
 library(dbarts)
 library(timetk)     # FOR EDA
+library(modeltime)
+library(forecast)
 
 ## Read in the data
 itemTrain <- vroom("Item-Demand-Challenge-IDC-/train.csv")
 itemTest <- vroom("Item-Demand-Challenge-IDC-/test.csv")
 
-storeItem <- itemTrain %>% 
+subtrain_1 <- itemTrain %>% 
   filter(store ==9, item ==36)
+subtrain_2 <- itemTrain %>% 
+  filter(store ==5, item ==50)
 
 # Pull out Holidays? Days of the Week? Month?
-ide_recipe <- recipe(sales ~., data = storeItem) %>% 
+ide_recipe_sub_1 <- recipe(sales ~., data = subtrain_1) %>% 
   step_date(date, features = c('dow','month','year','doy')) %>% 
   step_range(date_doy, min=0,max=pi) %>% 
   step_mutate(sinDOY=sin(date_doy), cosDOY=cos(date_doy)) %>% 
@@ -64,6 +68,132 @@ collect_metrics(CV_results) %>%
   filter(mtry == as.numeric(bestTune[1]), 
          min_n ==as.numeric(bestTune[2])) %>% 
   pull('mean')
+
+
+
+
+# ARIMA -------------------------------------------------------------------
+subtrain_1 <- itemTrain %>% 
+  filter(store ==9, item ==36) %>% 
+  select(-store,-item)
+
+subtrain1_test <- itemTest %>%
+  filter(store==9, item==36) %>%
+  select(-store,-item)
+
+subtrain_2 <- itemTrain %>% 
+  filter(store ==5, item ==50) %>% 
+  select(-store,-item)
+
+subtrain2_test <- itemTest %>%
+  filter(store==5, item==50) %>%
+  select(-store,-item)
+
+# Pull out Holidays? Days of the Week? Month?
+ide_recipe_sub_1 <- recipe(sales ~., data = subtrain_1) %>% 
+  step_date(date, features = c('dow','month','year','doy')) %>% 
+  step_range(date_doy, min=0,max=pi) %>% 
+  step_mutate(sinDOY=sin(date_doy), cosDOY=cos(date_doy)) %>% 
+  #step_lag(sales, lag = 7)
+  step_holiday(holidays = c("ChristmasDay"))
+
+# Pull out Holidays? Days of the Week? Month?
+ide_recipe_sub_2 <- recipe(sales ~., data = subtrain_2) %>% 
+  step_date(date, features = c('dow','month','year','doy')) %>% 
+  step_range(date_doy, min=0,max=pi) %>% 
+  step_mutate(sinDOY=sin(date_doy), cosDOY=cos(date_doy)) %>% 
+  #step_lag(sales, lag = 7)
+  step_holiday(holidays = c("ChristmasDay"))
+
+cv_split_1 <- time_series_split(subtrain_1, assess="3 months", cumulative = TRUE)
+cv_split_1 %>%
+  tk_time_series_cv_plan() %>% #Put into a data frame
+  plot_time_series_cv_plan(date, sales, .interactive=FALSE)
+
+cv_split_2 <- time_series_split(subtrain_2, assess="3 months", cumulative = TRUE)
+cv_split_2 %>%
+  tk_time_series_cv_plan() %>% #Put into a data frame
+  plot_time_series_cv_plan(date, sales, .interactive=FALSE)
+
+
+arima_recipe_1 <- ide_recipe_sub_1 # For the linear model part
+arima_recipe_2 <- ide_recipe_sub_2 # For the linear model part
+arima_model <- arima_reg(seasonal_period=365, # How long is the searson? 
+                         non_seasonal_ar=5, # default max p to tune
+                         non_seasonal_ma=5, # default max q to tune
+                         seasonal_ar=2, # default max P to tune
+                         seasonal_ma=2, #default max Q to tune
+                         non_seasonal_differences=2, # default max d to tune
+                         seasonal_differences=2 #default max D to tune
+                         ) %>%
+  set_engine("auto_arima")
+
+arima_wf_1 <- workflow() %>%
+  add_recipe(arima_recipe_1) %>%
+  add_model(arima_model) %>%
+  fit(data=training(cv_split_1))
+
+arima_wf_2 <- workflow() %>%
+  add_recipe(arima_recipe_2) %>%
+  add_model(arima_model) %>%
+  fit(data=training(cv_split_2))
+
+# Cross-validate to tune model
+cv_results_1 <- modeltime_calibrate(arima_wf_1,
+                                  new_data = testing(cv_split_1))
+
+cv_results_2 <- modeltime_calibrate(arima_wf_2,
+                                    new_data = testing(cv_split_2))
+
+## Visualize CV results
+cv_results_1 %>%
+  modeltime_forecast(new_data = testing(cv_split_1),
+                   actual_data = subtrain_1) %>%
+  plot_modeltime_forecast(.interactive=TRUE)
+
+cv_results_2 %>%
+  modeltime_forecast(new_data = testing(cv_split_2),
+                     actual_data = subtrain_2) %>%
+  plot_modeltime_forecast(.interactive=TRUE)
+
+## Evaluate the accuracy
+cv_results_1 %>%
+  modeltime_accuracy() %>%
+  table_modeltime_accuracy(.interactive = FALSE)
+
+cv_results_2 %>%
+  modeltime_accuracy() %>%
+  table_modeltime_accuracy(.interactive = FALSE)
+
+# Refit to all data then forecast
+arima_fullfit_1 <- cv_results_1 %>%
+  modeltime_refit(data = subtrain_1)
+
+arima_fullfit_2 <- cv_results_2 %>%
+  modeltime_refit(data = subtrain_2)
+
+arima_preds_1 <- arima_fullfit_1 %>%
+  modeltime_forecast(new_data = subtrain1_test) %>%
+  rename(date=.index, sales=.value) %>%
+  select(date, sales) %>%
+  full_join(., y=subtrain1_test, by="date") %>%
+  select(id, sales)
+
+arima_preds_2 <- arima_fullfit_2 %>%
+  modeltime_forecast(new_data = subtrain2_test) %>%
+  rename(date=.index, sales=.value) %>%
+  select(date, sales) %>%
+  full_join(., y=subtrain2_test, by="date") %>%
+  select(id, sales)
+
+arima_fullfit_1 %>%
+  modeltime_forecast(new_data = subtrain1_test, actual_data = subtrain_1) %>%
+  plot_modeltime_forecast(.interactive=FALSE)
+
+arima_fullfit_2 %>%
+  modeltime_forecast(new_data = subtrain2_test, actual_data = subtrain_2) %>%
+  plot_modeltime_forecast(.interactive=FALSE)
+
 
 
 # Double For Loop ---------------------------------------------------------
